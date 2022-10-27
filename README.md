6d7718c2ab26452539416fea70bd7e75415fb6b6
RuthNjeri6 <rthmbugua@gmail.com>
2022-10-27 13:03:38 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Gaze Tracker API
+An api that runs the gaze tracker model, Takes post request with a frame as body of request.
+
+# How to setup this API
+1. Create a virtual environment   
+   ```python3 -m venv name_of_your_environment```
+2. Install all the dependencies  
+   ```pip install -r requirements```
+3. Activate the environment  
+   ```source activate name_of_your_environment/bin/activate```
+4. Run the api  
+   ```python3 api/api.py```
+
+API should be running at [http://127.0.0.1:4000](http://127.0.0.1:4000)  
+Access the routes documentation from [http://127.0.0.1:4000/docs](http://127.0.0.1:4000/docs)  
---
+++ kivy_app/main.py
@@ -0,0 +1,92 @@
+from multiprocessing.pool import ThreadPool
+from kivy.app import App
+from kivy.uix.image import Image
+from kivy.graphics import Ellipse, Color
+from kivy.network.urlrequest import UrlRequest
+import cv2
+import json
+import requests
+from dotenv import load_dotenv
+import os
+
+class Touch(Image):
+    # only send frame on touch/click event
+    # add a button to save data collected
+    """on touch down, get cap and get frame"""
+    def on_touch_down(self, touch):
+        global video_frame, collected_x, collected_y
+        x = touch.x
+        y = touch.y
+        prediction = self.sendFrame(video_frame)
+        print('Prediction...', prediction)
+        if prediction is not None and len(prediction) == 28:
+            with self.canvas:
+                Color(1,0,0)
+                d = 20
+                Ellipse(pos=(x -d/2, y - d/2), size=(d, d))
+            collected_x.append(prediction)
+            collected_y.append((x,y))
+        video_frame = []
+
+    def sendFrame(self, img):
+        url = os.environ.get('URL') + '/predict'
+        payload = json.dumps({
+                    "frame": img,
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        req = UrlRequest(url=url, req_headers=headers, req_body=payload)
+        req.wait()
+        # response = UrlRequest(url=url, req_headers=headers, req_body=payload, ca_file=cfi.where(), verify=True)
+        data = req.result
+        return data['prediction']
+
+class MainApp(App):
+    def build(self):
+        return Touch()
+    def on_start(self):
+        self.save_current = False
+        self.pool = ThreadPool(processes=1)
+        async_start = self.pool.apply_async(self.getFrame)
+    
+    def getFrame(self):
+        global video_frame
+        cap = cv2.VideoCapture(0)
+        while True:
+            ret, image = cap.read()
+            if not ret:
+                print("Frame cannot be read. Exiting...")
+                break
+            video_frame = image.tolist()
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break 
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    def on_stop(self):
+        status = self.saveData()
+        if status:
+            # forcefully close all worker threads
+            print('data send successfully')
+            self.pool.terminate()
+
+    def saveData(self):
+        url = os.environ.get('URL') + '/save'
+        payload = json.dumps({
+                    "landmarks": collected_x,
+                    "labels": collected_y
+        })
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        data = json.loads(response.text)
+        status = data['status']
+        return status
+
+if __name__ == '__main__':
+    load_dotenv()
+    video_frame = []
+    collected_x = []
+    collected_y = []
+    MainApp().run()
